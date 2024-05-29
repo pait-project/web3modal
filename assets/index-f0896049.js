0aadaefca45080ea4945dfd52e698f355699fb43
v.kiriiak <valentyn.kiriiak@playtech.com>
2024-05-29 23:59:03 +0300

    Updates

--- assets/index-f0896049.js
+++ assets/index-f0896049.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-3ed38f9b.js";import{ap as Ur}from"./index-3d4378d7.js";/**
+import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-f0790e0a.js";import{ap as Ur}from"./index-96619ad0.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
